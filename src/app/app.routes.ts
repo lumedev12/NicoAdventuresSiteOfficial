7ffd6586fb1c734d98d943b2cacbdd4657190b0f
lumedev12 <lumedev12@gmail.com>
2026-03-30 21:49:52 -0300

    feat/modelo beta de arquivos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/home/home.component')
+        .then(m => m.HomeComponent)
+  },
+  {
+    path: 'sobre',
+    loadComponent: () =>
+      //import('./pages/sobre/sobre.component')
+        import('./pages/sobre/sobre.component')
+            .then(m => m.SobreComponent)
+  },
+  {
+    path: 'download',
+    loadComponent: () =>
+      //import('./pages/download/download.component')
+        import('./pages/download/download.component')
+        .then(m => m.DownloadComponent)
+  }
+];
